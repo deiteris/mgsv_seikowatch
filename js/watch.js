0c904493d40844d77410e49fcecca005386eb056
Snek <RIP95_95@mail.ru>
2015-10-10 19:30:32 +0300

    Code update

--- js/watch.js
+++ js/watch.js
@@ -1,16 +1,12 @@
 var todaydate = new Date();
 
-var _second = 1000;
-var _minute = _second * 60;
-var _hour = _minute * 60;
-
 var cigartime = 0;
-var intervaltime = 1000;
 var reverse_flag = false;
-
 var reloadnum = 30;
 var nowcount = 0;
 var tid = null;
+var init_flag = false;
+var getdate_flag = true;
 
 var second = {
   'w': 100,
@@ -25,24 +21,19 @@ var numl = {
   'h': 30
 };
 
-var init_flag = false;
-var getdate_flag = true;
-
 function get_content() {
 
-	if (getdate_flag) {
-	  getdate_flag = false;
+  if (getdate_flag) {
+    getdate_flag = false;
 
-	  nowcount = 0;
+    nowcount = 0;
 
-	  if (!init_flag) {
-		init_flag = true;
-		countdown();
-		$('.timerarea').css({
-		  'visibility': 'visible'
-		});
-	  }
-	}
+    if (!init_flag) {
+      init_flag = true;
+      countdown();
+      $('.timerarea').css({'visibility': 'visible'});
+    }
+  }
 }
 
 var basetimes = [60, 60, 24];
@@ -51,6 +42,7 @@ function getshowtimes() {
   var int_time = new Date() * 1;
   var cur_time = Math.round(int_time / 1000);
   var showtime = cur_time + cigartime + (60 * 60 * 3);
+
   if (showtime <= 0) showtime = 0;
 
   var rets = [];
@@ -74,6 +66,7 @@ function getshowtimes() {
     rets[ii] = tmp;
     showtime -= rets[ii] * buff;
   }
+
   return rets;
 }
 
@@ -84,46 +77,28 @@ var drawer = function() {
   for (var ii = 0; ii < 4; ii++) {
     var num2 = Math.floor(showtimes[ii] / 10);
     var num1 = showtimes[ii] % 10;
-	var days_hi = Math.floor(todaydate.getDate() / 10);
-	var days_lo = Math.floor(todaydate.getDate() % 10);
+    var days_hi = Math.floor(todaydate.getDate() / 10);
+    var days_lo = Math.floor(todaydate.getDate() % 10);
 
     switch (ii) {
       case 0:
-          var w = -900 + (second.w * num1);
-          var h = -500 + (second.h * num2);
-        $('.clock').css(
-          'background-position', w + 'px ' + h + 'px'
-        );
-        $('.sec2').css(
-          'background-position', (-nums.w * num2) + 'px 0px'
-        );
-        $('.sec1').css(
-          'background-position', (-nums.w * num1) + 'px 0px'
-        );
+        var w = -900 + (second.w * num1);
+        var h = -500 + (second.h * num2);
+        $('.clock').css('background-position', w + 'px ' + h + 'px');
+        $('.sec2').css('background-position', (-nums.w * num2) + 'px 0px');
+        $('.sec1').css('background-position', (-nums.w * num1) + 'px 0px');
         break;
       case 1:
-        $('.min2').css(
-          'background-position', (-numl.w * num2) + 'px 0px'
-        );
-        $('.min1').css(
-          'background-position', (-numl.w * num1) + 'px 0px'
-        );
+        $('.min2').css('background-position', (-numl.w * num2) + 'px 0px');
+        $('.min1').css('background-position', (-numl.w * num1) + 'px 0px');
         break;
       case 2:
-        $('.hour2').css(
-          'background-position', (-numl.w * num2) + 'px 0px'
-        );
-        $('.hour1').css(
-          'background-position', (-numl.w * num1) + 'px 0px'
-        );
+        $('.hour2').css('background-position', (-numl.w * num2) + 'px 0px');
+        $('.hour1').css('background-position', (-numl.w * num1) + 'px 0px');
         break;
       case 3:
-        $('.day2').css(
-          'background-position', (-numl.w * days_hi) + 'px 0px'
-        );
-        $('.day1').css(
-          'background-position', (-numl.w * days_lo) + 'px 0px'
-        );
+        $('.day2').css('background-position', (-numl.w * days_hi) + 'px 0px');
+        $('.day1').css('background-position', (-numl.w * days_lo) + 'px 0px');
         break;
     }
   }
@@ -147,7 +122,7 @@ var downer = function() {
 
 var countdown = function() {
   if (tid === null) {
-    tid = setInterval(downer, intervaltime);
+    tid = setInterval(downer, 1000);
   }
 };
 
@@ -179,24 +154,24 @@ var variable_cd = function() {
 
 function onHover()
 {
-    $('#cigar').attr('src', 'img/phantomcigar_a.jpg');
-	
-	var file = 'sounds/select.mp3';
-	
-	(function initAudio() {
-      var audio5js = new Audio5js({
-        ready: function() {
-          this.load(file);
-          this.play();
-        }
-      });
-      audioobj = audio5js;
-    })();
+  $('#cigar').attr('src', 'img/phantomcigar_a.jpg');
+
+  var file = 'sounds/select.mp3';
+
+  (function initAudio() {
+    var audio5js = new Audio5js({
+      ready: function() {
+        this.load(file);
+        this.play();
+      }
+    });
+    audioobj = audio5js;
+  })();
 }
 
 function offHover()
 {
-    $('#cigar').attr('src', 'img/phantomcigar.jpg');
+  $('#cigar').attr('src', 'img/phantomcigar.jpg');
 }
 
 $('.phantomcigar').click(function() {
@@ -220,9 +195,9 @@ $('.phantomcigar').click(function() {
     animtime = 0;
     variable_cd();
 
-	$('.phantomcigar').pulse({opacity: 0}, {duration : 100, pulses : 5});
-	setTimeout(function() {$('.phantomcigar').css({'display': 'none'});}, 500);
-	
+    $('.phantomcigar').pulse({opacity: 0}, {duration : 100, pulses : 5});
+    setTimeout(function() {$('.phantomcigar').css({'display': 'none'});}, 500);
+
     setTimeout(function() {
 
       cigartime = 0;
@@ -237,27 +212,15 @@ $('.phantomcigar').click(function() {
       get_content();
       countdown();
 
-      $('.timer').css({
-        'display': 'none'
-      });
-	  $('.timerbg').css({
-        'display': 'block'
-      });
+      $('.timer').css({'display': 'none'});
+      $('.timerbg').css({'display': 'block'});
 
       setTimeout(function() {
-        $('.timer').css({
-          'display': 'block'
-        });
-        $('.timerbg').css({
-          'display': 'none'
-        });
+        $('.timer').css({'display': 'block'});
+        $('.timerbg').css({'display': 'none'});
       }, 2000);
 
-      setTimeout(function() {
-        $('.phantomcigar').css({
-          'display': 'block'
-        });
-      }, 5000);
+      setTimeout(function() {$('.phantomcigar').css({'display': 'block'});}, 5000);
 
     }, max_animtime);
   }
