d5cd24fe5def1b11c9004133e066deb4f65381b6
RIP95 <rip95_95@mail.ru>
2017-03-19 21:45:37 +0300

    Update code

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Remake of seiko timer from Konami website with realtime clock function and worki
 
 Source code origin: http://s.codepen.io/michaellfoo/debug/mJOLMw
 
-Demo: http://solidsnekdemo.my1.ru/seiko/index.htm
+Demo: https://suneku.ru/examples/seiko/
--- js/watch.js
+++ js/watch.js
@@ -1,167 +1,184 @@
 /* Global variables declarations */
-var thetime = new Date();
-var cigartime = 0;
-var init_flag = false;
-var animtime = 0;
-var max_animtime = 9000;
-var cigar_timer = null;
-var old_time = 0;
+var cigarTime = 0;
+var initFlag = false;
+var animTime = 0;
+var maxAnimTime = 9000;
+var cigarFlag = null;
+var oldTime = 0;
 var audioobj = null;
+var timeUnit = {
+    milliseconds: 1000,
+    seconds: 60,
+    minutes: 60,
+    hours: 24
+};
 
 /* Code start */
 function getContent() {
-  if (!init_flag) {
-    init_flag = true;
-    countdown();
-    $('.dialarea').css({'visibility': 'visible'});
-  }
+    if (!initFlag) {
+        initFlag = true;
+        countdown();
+    }
 }
 
 function getTime() {
-  var cur_time = Math.round(new Date() * 1 / 1000);
-  var time = cur_time + cigartime + (60 * 60 * 3);
+    var theTime = new Date() * 1;
 
-  /*
-   * [0] - Seconds
-   * [1] - Minutes
-   * [2] - Hours
-   */
-  var basetime = [60, 60, 24];
-  var array = [];
+    var cur_time = Math.round(theTime / timeUnit.milliseconds);
+    var time = cur_time + cigarTime + (timeUnit.seconds * timeUnit.minutes * 3);
 
-  array[0] = time % basetime[0]; /* This should be minutes */
-  time -= array[0];
+    /*
+     * [0] - Seconds
+     * [1] - Minutes
+     * [2] - Hours
+     */
+    var basetime = [timeUnit.seconds, timeUnit.minutes, timeUnit.hours];
+    var array = [];
 
-  var buff = 1;
-  for (var ii = 1; ii <= 3; ii++) {
-    buff *= basetime[ii - 1];
-    var tmp = time / buff;
-    tmp = tmp % basetime[ii];
-    array[ii] = tmp;
-    time -= array[ii] * buff;
-  }
+    /* First get seconds and put them to array[0] */
+    array[0] = time % basetime[0];
+    time -= array[0];
 
-  return array;
+    var buff = 1;
+    /* Then get minutes and hours and put them to array[1] and [2] */
+    for (var i = 1; i <= 2; i++) {
+        buff *= basetime[i - 1];
+        var tmp = time / buff;
+        tmp = tmp % basetime[i];
+        array[i] = tmp;
+        time -= array[i] * buff;
+    }
+
+    /* Output array with 3 elements */
+    return array;
 }
 
 function draw() {
 
-  var time = getTime();
+    var theTime = new Date();
 
-  for (var ii = 0; ii < 4; ii++) {
-    var num2 = Math.floor(time[ii] / 10);
-    var num1 = time[ii] % 10;
-    var days2 = Math.floor(thetime.getDate() / 10);
-    var days1 = Math.floor(thetime.getDate() % 10);
-    var second = {'w': 100, 'h': 100};
-    var nums = {'w': 35, 'h': 25};
-    var numl = {'w': 45, 'h': 30};
+    var time = getTime();
 
-    switch (ii) {
-      case 0:
-        var w = -900 + (second.w * num1);
-        var h = -500 + (second.h * num2);
-        $('.clock').css('background-position', w + 'px ' + h + 'px');
-        $('.sec2').css('background-position', (-nums.w * num2) + 'px 0px');
-        $('.sec1').css('background-position', (-nums.w * num1) + 'px 0px');
-        break;
-      case 1:
-        $('.min2').css('background-position', (-numl.w * num2) + 'px 0px');
-        $('.min1').css('background-position', (-numl.w * num1) + 'px 0px');
-        break;
-      case 2:
-        $('.hour2').css('background-position', (-numl.w * num2) + 'px 0px');
-        $('.hour1').css('background-position', (-numl.w * num1) + 'px 0px');
-        break;
-      case 3:
-        $('.day2').css('background-position', (-numl.w * days2) + 'px 0px');
-        $('.day1').css('background-position', (-numl.w * days1) + 'px 0px');
-        break;
+    for (var i = 0; i < 4; i++) {
+        var ClockStatus = {
+            num2: Math.floor(time[i] / 10),
+            num1: time[i] % 10,
+            days2: Math.floor(theTime.getDate() / 10),
+            days1: Math.floor(theTime.getDate() % 10),
+            second: {'w': 100, 'h': 100},
+            nums: {'w': 35, 'h': 25},
+            numl: {'w': 45, 'h': 30}
+        };
+
+        switch (i) {
+            case 0:
+                var w = -900 + (ClockStatus.second.w * ClockStatus.num1);
+                var h = -500 + (ClockStatus.second.h * ClockStatus.num2);
+                $('.clock').css('background-position', w + 'px ' + h + 'px');
+                $('.sec2').css('background-position', (-ClockStatus.nums.w * ClockStatus.num2) + 'px 0px');
+                $('.sec1').css('background-position', (-ClockStatus.nums.w * ClockStatus.num1) + 'px 0px');
+                break;
+            case 1:
+                $('.min2').css('background-position', (-ClockStatus.numl.w * ClockStatus.num2) + 'px 0px');
+                $('.min1').css('background-position', (-ClockStatus.numl.w * ClockStatus.num1) + 'px 0px');
+                break;
+            case 2:
+                $('.hour2').css('background-position', (-ClockStatus.numl.w * ClockStatus.num2) + 'px 0px');
+                $('.hour1').css('background-position', (-ClockStatus.numl.w * ClockStatus.num1) + 'px 0px');
+                break;
+            case 3:
+                $('.day2').css('background-position', (-ClockStatus.numl.w * ClockStatus.days2) + 'px 0px');
+                $('.day1').css('background-position', (-ClockStatus.numl.w * ClockStatus.days1) + 'px 0px');
+                break;
+        }
     }
-  }
 }
 
 function counter() {
-  draw();
-  getContent();
+    draw();
+    getContent();
 }
 
 function countdown() {
-  setInterval(counter, 1000);
+    setInterval(counter, timeUnit.milliseconds);
 }
 
 /* Clock start */
 getContent();
 
-/* Calculate cigartime */
-function getCigartime() {
+/* Calculate cigarTime */
+function getcigarTime() {
 
-  if (old_time === 0) old_time = new Date() * 1;
+    var theTime = new Date() * 1;
 
-  var now_time = new Date() * 1;
+    if (oldTime === 0) {
+        oldTime = theTime;
+    }
 
-  animtime += now_time - old_time;
-  old_time = now_time;
+    var now_time = theTime;
 
-  var per = -Math.cos(8 * Math.PI / 8 * animtime / max_animtime) / 2 + 0.5;
-  if (1 < per) per = 1;
+    animTime += now_time - oldTime;
+    oldTime = now_time;
 
-  cigartime = Math.floor(per * 5000);
-  draw();
-  cigar_timer = setTimeout(getCigartime, 1);
+    var per = -Math.cos(8 * Math.PI / 8 * animTime / maxAnimTime) / 2 + 0.5;
+    if (1 < per) per = 1;
+
+    cigarTime = Math.floor(per * 5000);
+    draw();
+    cigarFlag = setTimeout(getcigarTime, 20);
 }
 
 /* Phantom cigar functionality */
 function onHover() {
-  (function initAudio() {
-    var audio5js = new Audio5js({
-      ready: function() {
-        this.load('sounds/select.mp3');
-        this.play();
-      }
-    });
-    audioobj = audio5js;
-  })();
+    (function initAudio() {
+        var audio5js = new Audio5js({
+            ready: function() {
+                this.load('sounds/select.mp3');
+                this.play();
+            }
+        });
+        audioobj = audio5js;
+    })();
 }
 
 $('.phantomcigar').click(function() {
 
-  (function initAudio() {
-    var audio5js = new Audio5js({
-      ready: function() {
-        this.load('sounds/cigar.mp3');
-        this.play();
-      }
-    });
-    audioobj = audio5js;
-  })();
+    (function initAudio() {
+        var audio5js = new Audio5js({
+            ready: function() {
+                this.load('sounds/cigar.mp3');
+                this.play();
+            }
+        });
+        audioobj = audio5js;
+    })();
 
-  old_time = 0;
-  animtime = 0;
-  getCigartime();
+    oldTime = 0;
+    animTime = 0;
+    getcigarTime();
 
-  $('.phantomcigar').pulse({opacity: 0}, {duration : 100, pulses : 5});
-  setTimeout(function() {$('.phantomcigar').css({'display': 'none'});}, 500);
-
-  setTimeout(function() {
-    cigartime = 0;
-
-    if (cigar_timer !== null) {
-      clearTimeout(cigar_timer);
-      cigar_timer = null;
-    }
-
-    countdown();
-
-    $('.dial').css({'display': 'none'});
-    $('.dialbg').css({'display': 'block'});
+    $('.phantomcigar').pulse({opacity: 0}, {duration : 100, pulses : 5});
+    setTimeout(function() {$('.phantomcigar').css({'display': 'none'});}, 500);
 
     setTimeout(function() {
-      $('.dial').css({'display': 'block'});
-      $('.dialbg').css({'display': 'none'});
-    }, 2000);
+        cigarTime = 0;
 
-    setTimeout(function() {$('.phantomcigar').css({'display': 'block'});}, 5000);
+        if (cigarFlag !== null) {
+            clearTimeout(cigarFlag);
+            cigarFlag = null;
+        }
 
-  }, max_animtime);
+        countdown();
+
+        $('.dial').css({'display': 'none'});
+        $('.dialbg').css({'display': 'block'});
+
+        setTimeout(function() {
+            $('.dial').css({'display': 'block'});
+            $('.dialbg').css({'display': 'none'});
+        }, timeUnit.milliseconds * 2);
+
+        setTimeout(function() {$('.phantomcigar').css({'display': 'block'});}, timeUnit.milliseconds * 5);
+
+    }, maxAnimTime);
 });
